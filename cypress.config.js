680c96134669c364c31b81f98713dfa19af83371
piosso1 <patrick2.udo@gmail.com>
2024-09-18 11:27:43 +0100

    test2

--- cypress.config.js
+++ cypress.config.js
@@ -4,12 +4,13 @@ module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
 
   e2e: {
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+
     baseUrl:
       "https://naveenautomationlabs.com/opencart/index.php?route=account/login",
     setupNodeEvents(on, config) {
       // implement node event listeners here ..
       require("cypress-mochawesome-reporter/plugin")(on);
-      specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}";
     },
   },
 
