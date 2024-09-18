b0573dbf36b9c63271dbff2aed839876808b0059
piosso1 <patrick2.udo@gmail.com>
2024-09-18 10:59:22 +0100

    specPatternUpdate

--- cypress.config.js
+++ cypress.config.js
@@ -9,6 +9,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require("cypress-mochawesome-reporter/plugin")(on);
+      specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}";
     },
   },
 
